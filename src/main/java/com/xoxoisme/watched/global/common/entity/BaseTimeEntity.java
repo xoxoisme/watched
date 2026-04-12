4fdec6c2306700bbc9af89c1ec9f1633ec3ac1f4
xoxoisme <kxoxo3333@gmail.com>
2026-04-12 16:31:42 +0900

    feat: updatedAt

--- src/main/java/com/xoxoisme/watched/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/xoxoisme/watched/global/common/entity/BaseTimeEntity.java
@@ -1,4 +1,12 @@
 package com.xoxoisme.watched.global.common.entity;
 
-public class BaseTimeEntity {
+import jakarta.persistence.Column;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseTimeEntity extends BaseEntity{
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
