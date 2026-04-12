1f2d63a12aff5ea58be795c0379bb6d07be4a7db
xoxoisme <kxoxo3333@gmail.com>
2026-04-12 16:25:01 +0900

    feat: auto id, createdAt

--- src/main/java/com/xoxoisme/watched/global/common/entity/BaseEntity.java
+++ src/main/java/com/xoxoisme/watched/global/common/entity/BaseEntity.java
@@ -1,4 +1,21 @@
 package com.xoxoisme.watched.global.common.entity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
 }
