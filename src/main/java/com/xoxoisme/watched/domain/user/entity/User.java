601189816e64186830a45a66bd06bcd567516ddd
xoxoisme <kxoxo3333@gmail.com>
2026-04-12 20:21:41 +0900

    add: User entity setting

--- src/main/java/com/xoxoisme/watched/domain/user/entity/User.java
+++ src/main/java/com/xoxoisme/watched/domain/user/entity/User.java
@@ -1,4 +1,33 @@
 package com.xoxoisme.watched.domain.user.entity;
 
-public class User {
+import com.xoxoisme.watched.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = true, length = 255, name = "profile_image")
+    private String profileImageUrl;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;    //TODO: 응답 포맷은 "YYYY-MM-DD" dto에서 설정
 }
