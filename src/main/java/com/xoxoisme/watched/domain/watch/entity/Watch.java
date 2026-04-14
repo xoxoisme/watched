d2d49bffb09139c01d364bc77aa396bb6bd4905f
xoxoisme <kxoxo3333@gmail.com>
2026-04-14 15:41:39 +0900

    feat: Add watch entity

--- src/main/java/com/xoxoisme/watched/domain/watch/entity/Watch.java
+++ src/main/java/com/xoxoisme/watched/domain/watch/entity/Watch.java
@@ -1,4 +1,27 @@
 package com.xoxoisme.watched.domain.watch.entity;
 
-public class Watch {
+import com.xoxoisme.watched.domain.content.entity.Content;
+import com.xoxoisme.watched.domain.user.entity.User;
+import com.xoxoisme.watched.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "watches")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Watch extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id", nullable = false)
+    private Content content;
+
+    @Column(nullable = false, length = 20)
+    private String status;  // 현재 시청 상태
 }
