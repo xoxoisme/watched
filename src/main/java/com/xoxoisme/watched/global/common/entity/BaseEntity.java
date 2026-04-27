ef816f4f6bee7cf0fee9aceb3e612f9c37e51c45
xoxoisme <kxoxo3333@gmail.com>
2026-04-27 15:16:15 +0900

    fix: Delete never used import

--- src/main/java/com/xoxoisme/watched/global/common/entity/BaseEntity.java
+++ src/main/java/com/xoxoisme/watched/global/common/entity/BaseEntity.java
@@ -2,10 +2,8 @@ package com.xoxoisme.watched.global.common.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
